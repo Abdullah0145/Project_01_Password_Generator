c66138346d3eeb0b466687ce0e7477ef82bd6428
Abdullah0145 <mdabdullah3887336@gmail.com>
2024-04-12 16:54:37 +0530

    Project_01_Password_Generator

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-//Custom attribute Syntax in JS//const a = document.querySelector("[data-lengthSlider]");
-const inputSlider = document.querySelector("[data-lengthSlider]");
-const lengthDisplay = document.querySelector("[data-lengthNumber]");
+// Custom attribute Syntax in JS//const a = document.querySelector("[data-lengthSlider]");
 const passwordDisplay = document.querySelector("[data-passwordDisplay]");
 const copyBtn = document.querySelector("[data-copy]");
 const  copyMsg = document.querySelector("[data-copyMsg]");
+const lengthDisplay = document.querySelector("[data-lengthNumber]");
+const inputSlider = document.querySelector("[data-lengthSlider]");
 // ID attribute Syntax in JS//const b = document.querySelector("#data-lengthSlider");
 const uppercaseCheck = document.querySelector("#uppercase");
 const lowercaseCheck = document.querySelector("#lowercase");
@@ -16,32 +16,43 @@ const generateBtn = document.querySelector(".generateButton");
 const allcheckBox = document.querySelectorAll("input[type=checkbox]");
 const symbols = '~`!@#$%^&*()_-+={[}]|:;"<,>.?/';
 
+// In starting password will be empty. 
 let password = "";
+// In starting PasswordLength will be equal 10.
 let passwordLength = 10;
+// In starting one checkbox will be tick.
 let checkCount = 1;
+// yaha pr mai handleSlider ko call kr diya.
 handleSlider();
-//set strength circle color to grey
+// set strength circle color to grey
 
-//set passwordLength
+// start function implement
+// set passwordLength - ye set kr deta hai password ki length ko
 function handleSlider() {
-    inputSlider.value = passwordLength;
-    lengthDisplay.innerText = passwordLength;
-    //or bhi kuch krna chahiye ? HW
+    inputSlider.value = passwordLength; // input slider hai, iski jo value hai ko mai chahta hu staring me, ye equal honge mere password ki length ke. means 10th ke equal ho gya.
+    lengthDisplay.innerText = passwordLength; // ko mai chahta hu lengthDisplay ke under innerText pra honga ye equal honge mere password ki length ke jiske under ham 10 rak dete.
+    // or bhi kuch krna chahiye ? HW
 }
 
+// color set krna strength ka , 
 function setIndicator(color) {
+    // indicator ke under jao, dot style me jao, iska jo bgcolor hai usko inputColor ke equal kr do.
     indicator.style.backgroundColor = color;
-    //shadow HW
+    // shadow HW shadow set krna strength ka
 }
 
+// getRndInteger me hamne two value pass kr diya min and max.
 function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
+    // Math.random() function kya krta hai? math.random() 0 se 1 ke beach me ek random number generate krte deta hai.
+    return Math.floor(Math.random() * (max - min)) + min; // Math.random() function krne se mera minimum no.(0) and maximum no.(1) hai un dono ke beach me kohi ek random number generate hoga. agar mai isko multiply kr do max - min ke sath ko ab apka answer kaha tak ayega 0 se leakr max - min tak aagya. posibility ho ke ye (Math.random() * (max - min) ek floating number ho sakta hai, ko mai kya kro isko? ko maine isko round off (Math.floor) kr diya for example: 6.4 aaya ko Answer hoga only 6, abhi bhi iski range kya hai? abhi bhi iski range 0 se lekar max - min hai, mujhe kya range chaiye? mujhe range chaiye min se max ko mai min plus(+) kr diya. ko isne hame min se max ke beach random number (integer) dega. isme 0 inclusive hota hai aur 1 exclusive hota hai. 
 }
 
+// mujhe ek random no. dalna hoga iske under password me single digit(0-9) ka ko kaha se kaha tak number chaiye hoga mujhe? 0 se 9 tak number chaiye hoga mujhe, because 0-9 single didgit number hota hai. isliye maine upper function me maine max aur min letha tha taki ab mai yaha pr fuction ke under old wala fuction ko use kr paoo getRndInteger(0,9), kon se range me? 0-9 ke range me lekar aoo.    
 function generateRandomNumber() {
     return getRndInteger(0,9);
 }
 
+// 
 function generateLowerCase() {
     return String.fromCharCode(getRndInteger(97,123))
 }
